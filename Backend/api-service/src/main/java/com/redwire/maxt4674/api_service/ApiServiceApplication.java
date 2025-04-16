2be61844add7bc7cde47fcee11a34f5801b9d477
Maxwell Taylor <S3931467@student.rmit.edu.au>
2025-04-17 00:46:23 +1000

    first commit

---
+++ Backend/api-service/src/main/java/com/redwire/maxt4674/api_service/ApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.redwire.maxt4674.api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceApplication.class, args);
+		System.out.println("Server Initialised");
+	}
+
+}
---
+++ Backend/api-service/src/test/java/com/redwire/maxt4674/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redwire.maxt4674.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
