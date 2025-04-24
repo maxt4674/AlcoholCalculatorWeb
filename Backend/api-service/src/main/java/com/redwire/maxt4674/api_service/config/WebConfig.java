d34cdee7a4344c46bcd34ce16852d14177cbcb75
maxt4674 <S3931467@student.rmit.edu.au>
2025-04-24 22:06:03 +1000

    add Supress warning

--- Backend/api-service/src/main/java/com/redwire/maxt4674/api_service/config/WebConfig.java
+++ Backend/api-service/src/main/java/com/redwire/maxt4674/api_service/config/WebConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SuppressWarnings("unused")
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  
